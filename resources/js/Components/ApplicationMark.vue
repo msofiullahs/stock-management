c54a9c6c51c674fb756ba2ff0a9d6e5bc823c43d
Muhammad Sofiullah Safriansyah <sofiullah.work@gmail.com>
2024-02-19 17:23:00 +0700

    fix responsive menu

--- resources/js/Components/ApplicationMark.vue
+++ resources/js/Components/ApplicationMark.vue
@@ -1,3 +1,4 @@
 <template>
-    <h3 class="fw-bold">Stock Management</h3>
+    <h3 class="fw-bold d-none d-sm-none d-md-block">Stock Management</h3>
+    <h3 class="fw-bold d-sm-block d-md-none">SM</h3>
 </template>
